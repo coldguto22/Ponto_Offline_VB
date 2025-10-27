a10050d0b0a490190e201128c0d1b925b1183a85
coldguto22 <otavio.beato@fatec.sp.gov.br>
2025-10-26 23:06:28 -0300

    Initialize Spring Boot project with Maven configuration, application properties, and basic structure

---
+++ ApiSpringboot/src/main/java/com/pontoofflineVB/pontoofflineAPI/PontoofflineApiApplication.java
@@ -0,0 +1,13 @@
+package com.pontoofflineVB.pontoofflineAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PontoofflineApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PontoofflineApiApplication.class, args);
+	}
+
+}
---
+++ ApiSpringboot/src/test/java/com/pontoofflineVB/pontoofflineAPI/PontoofflineApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pontoofflineVB.pontoofflineAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PontoofflineApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
